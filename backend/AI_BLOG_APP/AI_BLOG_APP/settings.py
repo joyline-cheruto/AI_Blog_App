37ed7a5e4579df4d2bfb01e322586213b3d34ead
joyline-cheruto <joycheruto832@gmail.com>
2024-09-26 13:26:44 +0300

    code refactor

--- backend/AI_BLOG_APP/AI_BLOG_APP/settings.py
+++ backend/AI_BLOG_APP/AI_BLOG_APP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h2b7qxbzr76oxd#&+e-40xr&#d=ymb&uay*_8$3kwerm3lt*fm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- backend/AI_BLOG_APP/blog_generator/views.py
+++ backend/AI_BLOG_APP/blog_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
 def index(request):
@@ -8,6 +10,24 @@ def login(request):
     return render(request, "login.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repeatpassword = request.POST["repeatpassword"]
+        
+        if password ==repeatpassword:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request, user)
+                return redirect("/")
+            except:
+                error_message = "Error creating Account!"
+                return render(request,"signup.html",{"error_message":error_message})
+        else:
+            error_message = "Password do not matc!"
+            return render(request,"signup.html",{"error_message":error_message})
     return render(request, "signup.html")
 
 def logout(request):
